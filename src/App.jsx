69f3013bb496b3087552d35461a3ae2b58034dfe
Andualem-Andy <andualemasamenew5@gmail.com>
2024-02-07 17:11:24 +0300

    css refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./app.scss"
-import Cursor from "./components/cursor/Cursor";
+//import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -8,10 +8,10 @@ import Services from "./components/services/Services";
 import Contact from "./components/contact/Contact";
 const App = () => {
   return <div>
-     <Cursor />
+     {/* <Cursor /> */}
       <section id="Homepage">
-        <Navbar />
-        <Hero />
+      <Navbar />
+      <Hero />
       </section>
       <section id="Services">
         <Parallax type="services" />
